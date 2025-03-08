27fda51be804e70762ae350e7bb4cca0059647b0
dimognetehem <dimognetehem@gmail.com>
2025-03-08 06:27:12 +0100

    fix: update AMI in ec2.tf to Ubuntu 22.04 and format iam.tf for consistency

--- Chatbot-UI/Jenkins-Server-TF/ec2.tf
+++ Chatbot-UI/Jenkins-Server-TF/ec2.tf
@@ -1,7 +1,7 @@
 # ec2.tf
 
 resource "aws_instance" "Jenkins-server" {
-  ami                         = "ami-05716d7e60b53d380" # Amazon Linux 2 Kernel 5.10 AMI 2.0.20250305.0 x86_64 HVM gp2
+  ami                         = "ami-0884d2865dbe9de4b" # Canonical, Ubuntu, 22.04, amd64 jammy image
   instance_type               = "t2.large"
   subnet_id                   = aws_subnet.jenkins_subnet_1.id
   associate_public_ip_address = true
